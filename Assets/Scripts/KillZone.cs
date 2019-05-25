8ac2ac3b78ad367ec9436098c06dc8bb868c0fcb
Chris <christian-boer@hotmail.com>
2019-05-25 02:15:48 +0200

    Basic killzone added

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
